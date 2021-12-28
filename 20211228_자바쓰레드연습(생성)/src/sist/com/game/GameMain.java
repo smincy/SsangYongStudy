94648f16a05630c439c98326ab2abbaad556f387
smincy <sminyc@DESKTOP-Walnut>
2021-12-28 20:04:22 +0900

    움직이는 말

--- 20211228_자바쓰레드연습(생성)/src/sist/com/game/GameMain.java
+++ 20211228_자바쓰레드연습(생성)/src/sist/com/game/GameMain.java
@@ -3,37 +3,42 @@ package sist.com.game;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+// 말이 자동으로 움직이게 하기위해 쓰레드
 
 // 윈도우
 public class GameMain extends JFrame {
+	// 상태표시줄에 메뉴들 만드는 과정
+	// 드롭다운 메뉴 안에 항목들이 JMenuItem
+
 	JMenuItem startItem = new JMenuItem("출발");
 	JMenuItem endItem = new JMenuItem("종료");
-	GameView g = new GameView();
+
+	GameView g = new GameView();	// GameView 가져오기
 
 	public GameMain() {
-		JMenu menu = new JMenu("게임");
-		menu.add(startItem);
+		JMenu menu = new JMenu("게임"); // '게임' 이라는 메뉴를 만들고
+		menu.add(startItem); // 그 메뉴를 누르면 하위항목으로 Item들 표시하도록 추가
 
 		menu.addSeparator(); // 구분선
 
 		menu.add(endItem);
-		JMenuBar bar = new JMenuBar();
+		JMenuBar bar = new JMenuBar(); // 메뉴들 전체를 묶어 주는(상태표시줄)것을 JMenuBar
 		bar.add(menu);
 
-		setJMenuBar(bar); // 윈도우 위에 메뉴 설정
+		setJMenuBar(bar); // 이걸 줘야 윈도우 위에 메뉴가 올라감
 
 		// 게임 추가
 		add("Center", g);
 
-		// 크기 설정
-		setSize(1000, 750);
-		setVisible(true);
-		setDefaultCloseOperation(EXIT_ON_CLOSE); // X 클릭시 메모리 해제
+		// 윈도우 크기 설정
+		setSize(700, 750);
+		setVisible(true); // 윈도우를 보여줘라
+		setDefaultCloseOperation(EXIT_ON_CLOSE); // 우상단 닫기버튼 [X] 클릭시 종료 - 메모리 해제
 
+		// 람다
 		startItem.addActionListener((e) -> {
-			g.start();
+			g.start();		
 		});
-
 		endItem.addActionListener((e) -> {
 			g.end();
 		});
--- 20211228_자바쓰레드연습(생성)/src/sist/com/game/GameView.java
+++ 20211228_자바쓰레드연습(생성)/src/sist/com/game/GameView.java
@@ -2,8 +2,9 @@ package sist.com.game;
 
 import java.awt.*;
 import javax.swing.*;
-// JFrame / JDialog / JWindow => 위에 올려서 사용
-// JPanel은 단독 수행이 불가능하다 (컴포넌트 묶어서 관리) => 화상
+
+// JFrame / JDialog / JWindow => 얘들 위에 올려서, 사용 가능하게 만드는게 JPanel
+// JPanel은 단독 수행이 불가능하다 (컴포넌트 묶어서 관리) => 화상, 버튼을 묶는
 
 public class GameView extends JPanel {
 	private HorseThread h = new HorseThread();
@@ -11,14 +12,18 @@ public class GameView extends JPanel {
 	private Image horse;
 
 	public GameView() {
-		horse = Toolkit.getDefaultToolkit().getImage("C:\\SY\\캡쳐\\i013521632353.gif"); // 이미지 가져오기
+		// 이미지 가져오기
+		horse = Toolkit.getDefaultToolkit().getImage("C:\\SY\\캡쳐\\i013521632353.gif");
 	}
 
 	// 그림을 그린다
-	public void paint(Graphics g) {
-		g.setColor(Color.white);
+	public void paint(Graphics g) { // Graphics => 그림을 그리는 도구
+
+		// 바탕색 설정
+		g.setColor(Color.black);
 		g.fillRect(0, 0, getWidth(), getHeight());
-		g.drawImage(horse, h.x, h.y, 100, 60, null);
+
+		g.drawImage(horse, h.x, h.y, 150, 80, this); // 그림, 가로위치, 세로위치, 크기, 높이, this
 	}
 
 	public void start() { // 시작
@@ -29,14 +34,13 @@ public class GameView extends JPanel {
 		h.interrupt();
 	}
 
+	
 	class HorseThread extends Thread {
-
-		int x = 850;
+		int x = 500;
 		int y = 600;
 
 		public void run() {
 			while (true) {
-
 				try {
 					x -= 5;
 					if (x < 0) { // x 가 0 보다 작아지면 쓰레드종료
@@ -44,12 +48,10 @@ public class GameView extends JPanel {
 						interrupt();
 						break;
 					}
-					Thread.sleep(300); // 잠깐씩 멈추게
+					Thread.sleep(200); // 잠깐씩 멈추게
 					repaint(); // 그림을 다시 그린다
 				} catch (Exception e) {
-
 				}
-
 			}
 		}
 	}
